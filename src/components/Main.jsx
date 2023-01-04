b79776a547907dd27092b0c0abe3771dd08540f3
ricardo junior <ricardojunior199502@gmail.com>
2023-01-04 12:17:18 -0300

    add fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,102 +1,105 @@
-import { 
-  MainTasks, 
-  Button, 
-  ButtonCreateTask, 
+import {
+  MainTasks,
+  Button,
+  ButtonCreateTask,
   HeaderTask,
-  TasksContainer
+  TasksContainer,
 } from "./Main.module";
 
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import plus from '../assets/plus.png'
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import plus from "../assets/plus.png";
 import { WithTask } from "./WithTask";
 import { WithoutTask } from "./WithoutTask";
 
-
-
-export function Main(){
-
+export function Main() {
   const [taskList, setTaskList] = useState([]);
 
-  const thereTask = taskList.length
+  const thereTask = taskList.length;
 
-  const totalTasks = taskList.length
+  const totalTasks = taskList.length;
 
-  const [newTask, setNewTask] = useState([])
+  const [newTask, setNewTask] = useState("");
 
-
-  function handleCreateNewTask(){
+  function handleCreateNewTask(event) {
     event.preventDefault();
-    setTaskList([...taskList, {
-      id: uuidv4(),
-      title: newTask,
-      isComplete: false
-    }])
-    setNewTask('')
+    setTaskList([
+      ...taskList,
+      {
+        id: uuidv4(),
+        title: newTask,
+        isComplete: false,
+      },
+    ]);
+    setNewTask("");
   }
 
-  function handleNewTaskChange (){
-    event.target.setCustomValidity('');
+  function handleNewTaskChange(event) {
+    event.target.setCustomValidity("");
     setNewTask(event.target.value);
   }
 
-  function deleteTask(taskdelited){
-    const delitedTask = taskList.filter((task) => task.id !== taskdelited)
-    setTaskList(delitedTask)
+  function deleteTask(taskdelited) {
+    const delitedTask = taskList.filter((task) => task.id !== taskdelited);
+    setTaskList(delitedTask);
   }
 
+  function itsComplete(checkedChanged, checkChangedComplete) {
+    const changeCheck = taskList.find((task) => task.id === checkedChanged);
+    const change = taskList.filter((task) => task.id !== checkedChanged);
 
- function itsComplete (checkedChanged, checkChangedComplete){
-  const checkTasks= taskList.filter((task) => task.id == checkedChanged) || taskList.filter((task) => task.isComplete == checkChangedComplete)
-  const changeCheck = checkTasks.find((task) => task.id === checkedChanged)
+    const newCheck = {
+      ...changeCheck,
+      isComplete: !changeCheck.isComplete,
+    };
 
-}
+    setTaskList([...change, newCheck]);
+  }
 
-
-  return(
-    
+  return (
     <TasksContainer>
       <MainTasks>
-        <Button onSubmit={handleCreateNewTask}  >
-          <textarea 
-            type="text" 
-            name="inputTask" 
+        <Button onSubmit={handleCreateNewTask}>
+          <textarea
+            type="text"
+            name="inputTask"
             placeholder="Adicione uma nova tarefa"
             value={newTask}
             onChange={handleNewTaskChange}
           />
-          <ButtonCreateTask 
-            type='submit' 
-          >
-              <span>Criar</span>
-              <img src={plus} alt=""/>
+          <ButtonCreateTask type="submit">
+            <span>Criar</span>
+            <img src={plus} alt="" />
           </ButtonCreateTask>
         </Button>
 
         <HeaderTask>
-            <p> Tarefas criadas <span>{thereTask}</span></p>
-            <p>Concluídas <span> 0 de {totalTasks} </span></p>
+          <p>
+            {" "}
+            Tarefas criadas <span>{thereTask}</span>
+          </p>
+          <p>
+            Concluídas <span> 0 de {totalTasks} </span>
+          </p>
         </HeaderTask>
 
-        { thereTask > 0 ? 
-          taskList.map((task,  i) =>{
+        {thereTask > 0 ? (
+          taskList.map((task, i) => {
             return (
-            <WithTask
-            key={task.id}
-            titulo={task.title}
-            isComplete={task.isComplete}
-            eliminar={task.isDelited}
-            deleteTask={() => deleteTask(task.id)}
-            itsComplete={() => itsComplete(task.id, task.isComplete )}
-            />
-          )
+              <WithTask
+                key={task.id}
+                titulo={task.title}
+                isComplete={task.isComplete}
+                eliminar={task.isDelited}
+                deleteTask={() => deleteTask(task.id)}
+                itsComplete={() => itsComplete(task.id, task.isComplete)}
+              />
+            );
           })
-          : 
-          <WithoutTask/>}
-
+        ) : (
+          <WithoutTask />
+        )}
       </MainTasks>
     </TasksContainer>
-    
-
-  )
+  );
 }
--- src/components/WithTask.jsx
+++ src/components/WithTask.jsx
@@ -1,14 +1,14 @@
-import { WithTasks } from './WithTask.module'
-import trash from '../assets/trash.svg'
-
+import { WithTasks } from "./WithTask.module";
+import trash from "../assets/trash.svg";
 
 export function WithTask(props) {
-
-  return(
-    <WithTasks isComplete={props.isComplete}   >
-      <input onClick={props.itsComplete}  type='checkbox'></input>
+  return (
+    <WithTasks isComplete={props.isComplete}>
+      <input onClick={props.itsComplete} type="checkbox"></input>
       <p>{props.titulo}</p>
-      <button onClick={props.deleteTask} ><img src={trash} /></button>
+      <button onClick={props.deleteTask}>
+        <img src={trash} />
+      </button>
     </WithTasks>
-  )
-} 
+  );
+}
